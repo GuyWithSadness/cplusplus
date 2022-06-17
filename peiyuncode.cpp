5279d7b1b642a3404077abc68eda98fd2fcab032
SaveMePlz <chuashixiang@gmail.com>
2022-06-17 17:54:13 +0800

    hm havent start yet

---
+++ peiyuncode.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string characterName = "John";
+    int characterAge;
+    characterAge = 35;
+    cout << "There once was a man named John" << endl;
+    return 0;
+}
