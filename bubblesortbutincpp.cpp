8839bf9328b4fd5da17b755f0f47aacbd07345d0
SpaghettiCodes <105684329+GuyWithSadness@users.noreply.github.com>
2022-06-23 19:39:00 +0800

    bubble sort

--- bubblesortbutincpp.cpp
+++ bubblesortbutincpp.cpp
@@ -1,18 +1,41 @@
-void bubblesort(int *tab, int size)
+#include <iostream>
+using namespace std;
+
+void bubblesort(int *tab)    
 {
+    int size;
+
+    size = sizeof(tab)/sizeof(tab[0]);
     int temp;
     int count;
-
-    for (count = 0; count < size - 1; count++)
+    int swapped;
+    
+    while (1)
     {
-        if (tab[count] > tab[count + 1])
+        swapped = 0;
+        for (count = 0; count < size - 1; count++)
         {
-            temp = tab[count];
-            tab[count] = tab[count + 1];
-            tab[count + 1] = temp;
-
-            count = 0;
+            if (tab[count] > tab[count + 1])
+            {
+                temp = tab[count];
+                tab[count] = tab[count + 1];
+                tab[count + 1] = temp;
+                swapped++;
+            }
         }
+        if(swapped == 0)
+            break;
     }
+
     return ;
+}
+
+int main()
+{
+    int tab[] = {2,2,1323,213,21,3,421,312,3,4,21,3}
+    int size = sizeof(tab)/sizeof(tab[0]);
+    for (int i = 0, i < size, i++)
+    {
+        cout << tab[i] << " ";
+    }
 }
